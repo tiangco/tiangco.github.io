c155dc70cb4e0362f700fd9b46e1f6a94940369b
Noel Tiangco <tiangco@technologist.com>
2020-09-30 16:46:11 -0400

    ras update
    ras

--- src/helpers/taskCard.js
+++
@@ -1,9 +0,0 @@
-export default class TaskCard {
-  constructor(scene) {
-      this.render = (x, y, sprite) => {
-          let card = scene.add.image(x, y, sprite).setScale(0.25).setInteractive();
-          scene.input.setDraggable(card);
-          return card;
-      }
-  }
-}
